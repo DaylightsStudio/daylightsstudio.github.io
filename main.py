ec2dd54601a83e1618f06c04afbe2af4e9ec2c1f
Maxime Vincent <43153657+Adaoh2@users.noreply.github.com>
2022-12-06 22:51:53 -0500

    ajouter creation fichier js

--- main.py
+++ main.py
@@ -2,16 +2,45 @@ import shutil
 import os
 
 def list_files(dir):
+
+    foreground = "{foreground}"
+    middleground = "{middleground}"
+    background = "{background}"
+
     for root, dirs, files in os.walk(dir):
         for d in dirs:
-            ancienEmplacement = str("./src/pages/" + d)
+            nomCarte = d[3:].replace(" ", "_").lower()
+            nomCarteCap = nomCarte.capitalize()
+
+            ancienEmplacement = str(f"./src/pages/{d}/")
             print(ancienEmplacement)
 
-            nouvelEmplacement = str("./src/pages/assets")
+            nouvelEmplacement = str(f"./src/pages/{nomCarte}/assets")
             print(nouvelEmplacement)
 
             os.makedirs(nouvelEmplacement)
-            shutil.move(ancienEmplacement, nouvelEmplacement)
+            os.rename(ancienEmplacement, nouvelEmplacement)
 
-#print(list_files("src/pages"))
+            f = open(f"{nomCarte}.js", "x")
+            f.write("""
+                import React from 'react';
+                import TiltCardWidget from '../../components/TiltCardWidget/TiltCardWidget';
+                import foreground from './assets/foreground.png';
+                import middleground from './assets/middleground.png';
+                import background from './assets/background.png';
+
+                function {nomCarteCap}() {
+                    return (
+                        <TiltCardWidget 
+                            fg={foreground}
+                            mg={middleground} 
+                            bg={background} /> 
+                    );
+                }
+
+                export default {nomCarteCap}};
+            """)
+            
+            f.close()
+
 list_files("src/pages")
