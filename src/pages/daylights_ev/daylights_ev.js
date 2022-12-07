646cac81aa36156b0a6eac949ee9b5676e051f85
Maxime Vincent <43153657+Adaoh2@users.noreply.github.com>
2022-12-07 18:45:44 -0500

    Fix Daylights-EV

--- src/pages/daylights_ev/daylights_ev.js
+++ src/pages/daylights_ev/daylights_ev.js
@@ -10,7 +10,8 @@ function Daylights_ev() {
         <TiltCardWidget 
             fg={foreground}
             mg={middleground} 
-            bg={background} /> 
+            bg={background} 
+            backgroundTransformCSS="translateZ(0px) translateY(0px) scale(1.2)"/> 
     );
 }
 
