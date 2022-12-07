ae90d3fd8dacf14ae0d4c134f6b68a9dc4cab8dd
Maxime Vincent <git@maximevincent.com>
2022-12-06 23:45:42 -0500

    deplacer fichiers

---
+++ src/pages/platii/platii.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import TiltCardWidget from '../../components/TiltCardWidget/TiltCardWidget';
+import foreground from './assets/foreground.png';
+import middleground from './assets/middleground.png';
+import background from './assets/background.png';
+
+function Platii() {
+    return (
+        <TiltCardWidget 
+            fg={foreground}
+            mg={middleground} 
+            bg={background} /> 
+    );
+}
+
+export default Platii;
+            
