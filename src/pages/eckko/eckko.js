5274a12b84576eb6aca50401c0b063cd64a75f1c
Maxime Vincent <git@maximevincent.com>
2022-12-09 00:20:41 -0500

    fix eckko

--- src/pages/eckko/eckko.js
+++ src/pages/eckko/eckko.js
@@ -10,7 +10,9 @@ function Eckko() {
         <TiltCardWidget 
             fg={foreground}
             mg={middleground} 
-            bg={background} /> 
+            bg={background} 
+            middlegroundTransformCSS = "translateZ(25px) translateY(-20px) scale(1.1)"
+            backgroundTransformCSS = "translateZ(0px) translateY(-20px) translateX(-30px) scale(1.1)" /> 
     );
 }
 
