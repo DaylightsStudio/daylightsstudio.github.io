178fb0b8bebe7b1525457b62b3ffe831cdebd5fd
Maxime V <43153657+Adaoh2@users.noreply.github.com>
2022-12-16 15:33:16 -0500

    ajouter template de base pour cartes

---
+++ src/routes/carte.jsx
@@ -0,0 +1,15 @@
+export default function Carte() {
+    return (
+      <>
+        <div>
+          <h1>Router</h1>
+          <nav>
+            <ul>
+              {}
+              <li><button><a href={`liqueur`}>Liqueur</a></button></li>
+            </ul>
+          </nav>
+        </div>
+      </>
+    );
+  }
