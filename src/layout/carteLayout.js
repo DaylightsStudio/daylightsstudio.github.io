15f8aae66acf097c860ea8a992680855e55e6b0f
Maxime Vincent <git@maximevincent.com>
2022-12-18 22:49:58 -0500

    Afficher la carte selon le parametre

--- src/layout/carteLayout.js
+++ src/layout/carteLayout.js
@@ -1,68 +1,70 @@
 import jsonData from '../components/descriptions/operateurs.json'
+import Carte from '../pages/Carte';
 
-function CarteLayout(props) {
+const windowUrl = window.location.search;
+const params = new URLSearchParams(windowUrl);
+const nomCarte = params.get('n');
+
+function CarteLayout() {
     return (
-    <div>
-                <div>
-            <h1 id='nomCarte'>{jsonData["adaoh"]["nom"]}</h1>
-            <p>{jsonData["adaoh"]["description"]["bio"]}</p>
-            <div className="container">
-                <div className="item" id="carte">
+        <div>
+            <Carte nomCarte={nomCarte} />
+            <div>
+                <h1 id='nomCarte'>{jsonData["adaoh"]["nom"]}</h1>
+                <p>{jsonData["adaoh"]["description"]["bio"]}</p>
+                <div className="container">
+                    <div className="item" id="carte">
 
-                </div>
+                    </div>
 
-                <div className="item" id="description">
+                    <div className="item" id="description">
 
-                <details>
-                    <summary>
-                        Attaque normale
-                    </summary>
-                    <table>
-                        <tr>
-                            <td><b>Nom</b></td>
-                            <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["titre"]}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Description</b></td>
-                            <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["description"]}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Dommages</b></td>
-                            <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["numero"]}</td>
-                        </tr>
-                    </table>
-                </details>
+                        <details>
+                            <summary>
+                                Attaque normale
+                            </summary>
+                            <table>
+                                <tr>
+                                    <td><b>Nom</b></td>
+                                    <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["titre"]}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Description</b></td>
+                                    <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["description"]}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Dommages</b></td>
+                                    <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["numero"]}</td>
+                                </tr>
+                            </table>
+                        </details>
 
-                <details>
-                    <summary>
-                        Attaque spéciale
-                    </summary>
-                    <table>
-                        <tr>
-                            <td><b>Nom</b></td>
-                            <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["titre"]}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Description</b></td>
-                            <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["description"]}</td>
-                        </tr>
-                        <tr>
-                            <td><b>Dommages</b></td>
-                            <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["numero"]}</td>
-                        </tr>
-                    </table>
-                </details>
+                        <details>
+                            <summary>
+                                Attaque spéciale
+                            </summary>
+                            <table>
+                                <tr>
+                                    <td><b>Nom</b></td>
+                                    <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["titre"]}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Description</b></td>
+                                    <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["description"]}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Dommages</b></td>
+                                    <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["numero"]}</td>
+                                </tr>
+                            </table>
+                        </details>
 
 
+                    </div>
                 </div>
             </div>
         </div>
-
-        {
-            props.children
-        }
-    </div>
-);
+    );
 
 }
 
