6956dc345f634c471c832aca433b0432c7fd0ad8
Maxime Vincent <git@maximevincent.com>
2022-12-18 20:13:24 -0500

    Ajouter lle debut de layout de la page dune carte

--- src/layout/carteLayout.js
+++ src/layout/carteLayout.js
@@ -1,10 +1,62 @@
+import jsonData from '../components/descriptions/operateurs.json'
+
 function CarteLayout(props) {
     return (
     <div>
-        <ul>
-            <li>Home</li>
-            <li>Users</li>
-        </ul>
+                <div>
+            <h1 id='nomCarte'>{jsonData["adaoh"]["nom"]}</h1>
+            <p>{jsonData["adaoh"]["description"]["bio"]}</p>
+            <div className="container">
+                <div className="item" id="carte">
+
+                </div>
+
+                <div className="item" id="description">
+
+                <details>
+                    <summary>
+                        Attaque normale
+                    </summary>
+                    <table>
+                        <tr>
+                            <td><b>Nom</b></td>
+                            <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["titre"]}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Description</b></td>
+                            <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["description"]}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Dommages</b></td>
+                            <td>{jsonData["adaoh"]["carte"]["attaques"]["basique"]["numero"]}</td>
+                        </tr>
+                    </table>
+                </details>
+
+                <details>
+                    <summary>
+                        Attaque spéciale
+                    </summary>
+                    <table>
+                        <tr>
+                            <td><b>Nom</b></td>
+                            <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["titre"]}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Description</b></td>
+                            <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["description"]}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Dommages</b></td>
+                            <td>{jsonData["adaoh"]["carte"]["attaques"]["speciale"]["numero"]}</td>
+                        </tr>
+                    </table>
+                </details>
+
+
+                </div>
+            </div>
+        </div>
 
         {
             props.children
