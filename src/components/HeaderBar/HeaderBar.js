a9989c7649eb5ee4acc3ac0e9a5d300102c0e908
Maxime Vincent <git@maximevincent.com>
2022-12-19 23:41:52 -0500

    Formatter les fichiers

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -7,40 +7,38 @@ import Logo from './Titans_logo.png'
 // // // // // // // // // // // // // // // -=|=- \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\
 
 export default function HeaderBar() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false)
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
 
-  return (
-    <nav className="navigation">
-        
-      <button
-        className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded)
-        }}>
-      </button>
+    return (
+        <nav className="navigation">
 
-      <img src="../../Titans_logo.png"></img>
+            <button
+                className="hamburger"
+                onClick={() => {setIsNavExpanded(!isNavExpanded)}}>
+            </button>
 
-      <div
-        className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }>
             <a href="/" id="logo">
                 <img src={Logo} height="40px" alt="Logo Titans" />
             </a>
+            
+            <div
+                className={ isNavExpanded ? "navigation-menu expanded" : "navigation-menu" }>
 
-        <ul>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+                <ul>
+                    <li>
+                        <a href="/">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="/cartes">Cartes</a>
+                    </li>
+                    <li>
+                        <a href="/faq">FAQ</a>
+                    </li>
+                    <li>
+                        <a href="/apropos">À propos</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
