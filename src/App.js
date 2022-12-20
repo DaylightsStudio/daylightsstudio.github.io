1a30c764e217d17c79626aac6641c0e02f928d6a
Maxime Vincent <git@maximevincent.com>
2022-12-20 15:28:37 -0500

    Refaire les routeurs et page d'accueil

--- src/App.js
+++ src/App.js
@@ -3,13 +3,42 @@ import React from "react";
 import Accueil from './pages/Accueil'
 import { ChakraProvider } from '@chakra-ui/react'
 import HeaderBar from './components/HeaderBar/HeaderBar';
+import Root from './routes';
+import CarteLayout from './layout/carteLayout'
+import Cartes from './pages/Cartes'
+import {
+    createBrowserRouter,
+    RouterProvider,
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Accueil/>,
+    },
+    {
+        path: "/carte",
+        element: <CarteLayout/>,
+    },
+    {
+      path: "/cartes",
+      element: <Cartes/>,
+    }
+]);
 
 function App() {
   return (
     // <ChakraProvider resetCSS theme={theme}>
     <div className='App'>
       <HeaderBar/>
-      <Accueil />
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
       
       
